9fdb91f9b7681d4a867772f38a7bfabd8dadfe84
naveed <naveed.ajaj@gmail.com>
2016-08-27 20:35:53 +0530

    init

--- Logger.py
+++ Logger.py
@@ -2,10 +2,8 @@ import thread
 import time
 from Tkinter import *
 import ttk
-
-from gpiozero import LED
-from time import sleep
-
+import RPi.GPIO as GPIO
+ 
 appview = __import__('AppMainView')
 app = appview.AppMainView()
 
@@ -13,11 +11,19 @@ app = appview.AppMainView()
 # Define a function for the thread
 def print_time(threadName, delay):
    count = 0
+   GPIO.setmode(GPIO.BCM)
+   GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+   GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
    while count < 15: 
       time.sleep(delay)
       count += 1
       app.macstatus.set("%s: %s :Counter %s" % (threadName, time.ctime(time.time()),str(count)))
       print "%s: %s" % (threadName, time.ctime(time.time()))
+      if(GPIO.input(23) ==1):
+        print(“Button 1 pressed”)
+      if(GPIO.input(24) == 0):
+        print(“Button 2 pressed”)
+        GPIO.cleanup()
 
 # Run Thread 
 try:
